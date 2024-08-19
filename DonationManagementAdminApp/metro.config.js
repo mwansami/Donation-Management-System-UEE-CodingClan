8194922fb19adfc6ec9d735c6fb951c15583552b
mwansami <mmchibanda14@gmail.com>
2024-08-19 16:46:35 +0200

    first-step

--- DonationManagementAdminApp/metro.config.js
+++ DonationManagementAdminApp/metro.config.js
@@ -1,10 +1,12 @@
+// metro.config.js
+const { getDefaultConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
 module.exports = {
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: true,
-                inlineRequires: true,
-            },
-        }),
-    },
+  ...defaultConfig,
+  resolver: {
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx'], // Add other extensions if needed
+  },
 };
+ 
